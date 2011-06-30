439380a4e9483485947b25b0f0e34da758fb2c6a
Pascal Borreli <pascal@borreli.com>
2011-07-01 01:23:32 +0200

    Cosmetic change

--- data/generator/sfDoctrineModule/hadori/template/templates/exportSuccess.php
+++ data/generator/sfDoctrineModule/hadori/template/templates/exportSuccess.php
@@ -6,27 +6,29 @@
   
   <h1><?php echo $this->renderHtmlText($this->get('export_title')) ?></h1>
 
-  <div>
+  <div id="export-form">
     [?php include_partial('<?php echo $this->getModuleName() ?>/export_form', array('helper' => $helper)) ?]
   </div>
 
 <?php if ($help = $this->get('export_help')): ?>
-  <div class='help'><?php echo $this->renderHtmlText($help) ?></div>
-
+  <div class="help"><?php echo $this->renderHtmlText($help) ?></div>
 <?php endif ?>
+    
+  <?php if ($this->configuration->hasExportFilterForm()): ?>
+  <div class="filters[?php echo $helper->isActiveFilter() ? ' active':'' ?]">
+    [?php include_partial('<?php echo $this->getModuleName() ?>/filters', array('form' => $filters, 'helper' => $helper)) ?]
+  </div>
+  <?php endif; ?>
+
   <div class="export-preview">
     [?php include_partial('<?php echo $this->getModuleName() ?>/list', array('pager' => $pager, 'helper' => $helper)) ?]
     <ul class="actions">
 <?php foreach ($this->configuration->getValue('export.actions') as $name => $params): ?>
-      <?php echo $this->linkTo($name, $params) ?>
+      <li><?php echo $this->linkTo($name, $params) ?></li>
   
 <?php endforeach; ?>    
     </ul>
   </div> 
 
-<?php if ($this->configuration->hasExportFilterForm()): ?>
-  <div class="filters[?php echo $helper->isActiveFilter() ? ' active':'' ?]">
-    [?php include_partial('<?php echo $this->getModuleName() ?>/filters', array('form' => $filters, 'helper' => $helper)) ?]
-  </div>
-<?php endif; ?>
+
 </div>
